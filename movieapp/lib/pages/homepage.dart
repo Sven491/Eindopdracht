e21f86f1fa8172721cafbf215a8a762daf1a3055
Sven491 <kleintechneutje@gmail.com>
2025-10-08 22:48:38 +0200

    removed poep

--- movieapp/lib/pages/homepage.dart
+++ movieapp/lib/pages/homepage.dart
@@ -29,7 +29,7 @@ class HomePage extends StatelessWidget {
         ],
       ),
     ),
-      appBar: AppBar(title: Text('Home testagain Page'),),
+      appBar: AppBar(title: Text('Home poep Page'),),
           body: Center(
             child: Row(
               children: [
